d4934c4469b91987c96d342cbb7be83f1d97b447
linusromland <48645689+linusromland@users.noreply.github.com>
2020-03-24 16:44:17 +0100

    updated some stuff

--- src/client.java
+++ src/client.java
@@ -9,6 +9,8 @@ public class client
         Scanner input = new Scanner(System.in);
         System.out.println("What ip is your server running at?");
         String servername = input.nextLine();
+        System.out.println("What your username?");
+        String username = input.nextLine();
         Socket sock = new Socket(servername, 8545);
         BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
         OutputStream ostream = sock.getOutputStream();
@@ -28,7 +30,7 @@ public class client
                     try {
                         String receiveMessage;
                         if((receiveMessage = receiveRead.readLine()) != null) {
-                            System.out.println("From Client: " + receiveMessage);
+                            System.out.println(receiveMessage);
                         }
                     } catch (IOException e) {
                         e.printStackTrace();
@@ -40,7 +42,7 @@ public class client
 
         while (true) {
             if ((sendMessage = keyRead.readLine()) != null) {
-                pwrite.println(sendMessage);
+                pwrite.println("From " + username + ": " +sendMessage);
                 pwrite.flush();
             }
         }
--- src/server.java
+++ src/server.java
@@ -1,27 +1,33 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
+
 public class server
 {
 
     public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
         ServerSocket sersock = new ServerSocket(8545);
         System.out.println("server running...");
         Socket sock = sersock.accept( );
-        BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
-        OutputStream ostream = sock.getOutputStream();
-        PrintWriter pwrite = new PrintWriter(ostream, true);
+                        BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
+                        OutputStream ostream = sock.getOutputStream();
+                        PrintWriter pwrite = new PrintWriter(ostream, true);
 
-        InputStream istream = sock.getInputStream();
-        BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
+                        InputStream istream = sock.getInputStream();
+                        BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
 
-        Thread t = new Thread(new Runnable(){
-            @Override
-            public void run() {
-                while(true){
-                    try {
+                        System.out.println("What your username?");
+                        String username = input.nextLine();
+
+                        Thread t = new Thread(new Runnable(){
+                            @Override
+                            public void run() {
+                                while(true){
+                                    try {
                         String receiveMessage;
                         if((receiveMessage = receiveRead.readLine()) != null) {
-                            System.out.println("From Client: " + receiveMessage);
+                            System.out.println(receiveMessage);
                         }
                     } catch (IOException e) {
                         e.printStackTrace();
@@ -34,7 +40,7 @@ public class server
         while (true) {
             String sendMessage = "no";
             if ((sendMessage = keyRead.readLine()) != null) {
-                pwrite.println(sendMessage);
+                pwrite.println("From " + username + ": " + sendMessage);
                 pwrite.flush();
             }
         }
